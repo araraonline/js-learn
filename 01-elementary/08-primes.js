3eb62b4e7aa2e27d9d7cb8b40d72f9637bad7aa9
araraonline <andre0lra@gmail.com>
2019-01-24 18:51:27 -0200

    EXERCISE ELEM Primes

---
+++ 01-elementary/08-primes.js
@@ -0,0 +1,16 @@
+let foundPrimes = [];
+function isPrime(number) {
+    for (let divisor of foundPrimes) {
+        if (number % divisor === 0) {
+            return false;
+        }
+    }
+    foundPrimes.push(number);
+    return true;
+}
+
+for (let n = 2;; n++) {
+    if (isPrime(n)) {
+        console.log(n);
+    }
+}
