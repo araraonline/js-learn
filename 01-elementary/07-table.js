5866f3b31ca2dc64bf774c39d70b717a9e56686a
araraonline <andre0lra@gmail.com>
2019-01-24 18:47:19 -0200

    EXERCISE ELEM Multiplication table

---
+++ 01-elementary/07-table.js
@@ -0,0 +1,7 @@
+for (let first = 1; first <= 12; first++) {
+    for (let second = 1; second <= 12; second++) {
+        let product = first * second;
+        console.log(`${first} X ${second} = ${product}`);
+    }
+    console.log(" ");
+}
