9cd675a6c0dfb39ccccdfdc294453be4dca6be8f
araraonline <andre0lra@gmail.com>
2019-01-30 11:43:57 -0200

    EXERCISE LISTS Binary search (clean)

--- 02-lists/18-binary.js
+++ 02-lists/18-binary.js
@@ -24,17 +24,14 @@ function tests() {
         return array[pos];
     }
 
-
     check(binarySearch(0, []), -1);
     check(binarySearch(0, [1, 2, 3, 4]), -1);
-
     check(binarySearch(1, [1, 2, 3, 4]), 0);
     check(binarySearch(2, [1, 2, 3, 4]), 1);
     check(binarySearch(3, [1, 2, 3, 4]), 2);
     check(binarySearch(4, [1, 2, 3, 4]), 3);
 
     for (let count = 1; count <= 100; count++) {
-
         // create sorted array
         let array = new Array(count);
         for (let i = 0; i < count; i++) {
@@ -42,9 +39,8 @@ function tests() {
         }
         array = array.sort((a, b) => a - b);
 
-        element = randomElement(array);
+        let element = randomElement(array);
         check(array[binarySearch(element, array)], element);
-
     }
 
     console.log("tests ok");
