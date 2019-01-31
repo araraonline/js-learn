d846adb67cdc61da1b99d1895493cfc50e814187
araraonline <andre0lra@gmail.com>
2019-01-31 16:42:33 -0200

    EXERCISE INTERMEDIATE Operators (clean)

--- 03-intermediate/01-operators.js
+++ 03-intermediate/01-operators.js
@@ -1,17 +1,21 @@
-for (let n = 0; n < 3 ** 8; n++) {
-    let operators = n.toString(3).padStart(8, 0);
+function generateExpression(seed) {
+    let operators = seed.toString(3).padStart(8, 0);
+
     let expression = "1";
     for (let i = 0; i < operators.length; i++) {
-        let operator = operators[i];
-
-        if (operator === "1") {
+        if (operators[i] === "1") {
             expression += "-";
-        } else if (operator === "2") {
+        } else if (operators[i] === "2") {
             expression += "+";
         }
         expression += i + 2;
     }
 
+    return expression
+}
+
+for (let n = 0; n < 3 ** 8; n++) {
+    let expression = generateExpression(n);
     if (eval(expression) === 100) {
         console.log(expression);
     }
