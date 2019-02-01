f14d3c5722e107cc3360ad4e57de10138da56b47
araraonline <andre0lra@gmail.com>
2019-02-01 14:11:10 -0200

    EXERCISE INTERMEDIATE Graph

--- 03-intermediate/03-graph.js
+++ 03-intermediate/03-graph.js
@@ -1,73 +1,156 @@
+let LINK_PREFIX = "LINK__";
+
 class Graph {
-    addPoint(name) {
+    constructor() {
+        this.links = new Map();
+        this.values = new Map();
     }
 
-    removePoint(name) {
+    // points and edges (links)
+
+    hasPoint(point) {
+        return this.links.has(point);
     }
 
-    getPoints() {
+    hasLink(a, b) {
+        return this.getLinksFrom(a).has(b);
+    }
+
+    addPoint(point) {
+        if (this.hasPoint(point))
+            return;
+
+        this.links.set(point, new Set());
     }
 
     addLink(a, b) {
+        if (this.hasLink(a, b))
+            return;
+
+        this.addPoint(a);
+        this.addPoint(b);
+        this.links.get(a).add(b);
+        this.links.get(b).add(a);
     }
 
-    deleteLink(a, b) {
+    removePoint(point) {
+        if (!this.hasPoint(point))
+            return;
+
+        let neighbors = this.links.get(point);
+        neighbors.forEach(n => this.removeLink(point, n));
+        this.links.delete(point);
+        this.removePointValue(point);
     }
 
-    getLinks(from) {
+    removeLink(a, b) {
+        if (!this.hasLink(a, b))
+            return;
+
+        this.links.get(a).delete(b);
+        this.links.get(b).delete(a);
+        this.removeLinkValue(a, b);
     }
 
-    setValue(element, value) {
+    getPoints() {
+        return Array.from(this.links.keys()).sort();
     }
 
-    getValue(element) {
+    getLinksFrom(point) {
+        return this.links.get(point) || new Set();
+    }
+
+    // values
+
+    setPointValue(point, value) {
+        if (!this.hasPoint(point))
+            return false;
+
+        this.values.set(point, value);
+        return true;
+    }
+
+    setLinkValue(a, b, value) {
+        if (!this.hasLink(a, b))
+            return false;
+
+        let key = LINK_PREFIX + [a, b];
+        this.values.set(key, value);
+        return true;
+    }
+
+    removePointValue(point) {
+        this.values.delete(point);
+    }
+
+    removeLinkValue(a, b) {
+        let key = LINK_PREFIX + [a, b];
+        this.values.delete(key);
+    }
+
+    getPointValue(point) {
+        return this.values.get(point);
+    }
+
+    getLinkValue(a, b) {
+        let key = LINK_PREFIX + [a, b];
+        return this.values.get(key);
     }
 }
 
 
 function tests() {
     function check(a, b) {
+        if (a && a.constructor === Set) {
+            return check(Array.from(a).sort(), Array.from(b).sort());
+        }
+        
         if (JSON.stringify(a) !== JSON.stringify(b)) {
+            console.log(a, b);
             throw new Error();
         }
     }
 
     g = new Graph();
-    g = g.addPoint("a");
+    g.addPoint("a");
     check(g.getPoints(), ["a"]);
 
     g = new Graph();
-    g = g.addPoint("a");
-    g = g.removePoint("a");
+    g.addPoint("a");
+    g.removePoint("a");
     check(g.getPoints(), []);
 
     g = new Graph();
-    g = g.addLink("a", "b");
+    g.addLink("a", "b");
     check(g.getPoints(), ["a", "b"]);
-    check(g.getLinks("a"), ["b"]);
-    check(g.getLinks("b"), ["a"]);
+    check(g.getLinksFrom("a"), new Set(["b"]));
+    check(g.getLinksFrom("b"), new Set(["a"]));
 
     g = new Graph();
-    g = g.addLink("a", "b");
-    g = g.removeLink("a", "b");
+    g.addLink("a", "b");
+    g.removeLink("a", "b");
     check(g.getPoints(), ["a", "b"]);
-    check(g.getLinks("a"), []);
-    check(g.getLinks("b"), []);
+    check(g.getLinksFrom("a"), new Set([]));
+    check(g.getLinksFrom("b"), new Set([]));
 
     g = new Graph();
-    g = g.addLink("a", "b");
-    g.setValue("a", 1);
-    g.setValue("b", 2);
-    g.setValue(["a", "b"], 3);
-    check(g.getValue("a"), 1);
-    check(g.getValue("b"), 2);
-    check(g.getValue(["a", "b"]), 3);
+    g.addLink("a", "b");
+    g.setPointValue("a", 1);
+    g.setPointValue("b", 2);
+    g.setLinkValue("a", "b", 3);
+    check(g.getPointValue("a"), 1);
+    check(g.getPointValue("b"), 2);
+    check(g.getLinkValue("a", "b"), 3);
 
     g = new Graph();
-    d.addPoint("a");
-    check(g.getValue("a"), undefined);
+    g.addPoint("a");
+    check(g.getPointValue("a"), undefined);
 
     g = new Graph();
-    check(g.getValue("a"), null);
-    check(g.getValue(["a", "b"]), null);
+    check(g.getPointValue("a"), undefined);
+    check(g.getLinkValue("a", "b"), undefined);
+
+    console.log("tests ok");
 }
+
+tests();
