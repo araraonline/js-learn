4d8cea95155a99d61c83fc729e635259ec5f9365
araraonline <andre0lra@gmail.com>
2019-02-02 11:39:27 -0200

    DOC 01/04-sum

--- 01-elementary/04-sum.js
+++ 01-elementary/04-sum.js
@@ -12,6 +12,7 @@ rl.question("Choose a number: ", number => {
 });
 
 function sumFromTo(from, to) {
+    /* Sum integers from 'from' to 'to' (inclusive) */
     let summation = 0;
     for (let n = from; n <= to; n++) {
         summation += n;
