86ee04b7bed22acfc3df9476e779e5b90104d0c6
araraonline <andre0lra@gmail.com>
2019-02-02 11:44:50 -0200

    DOC 01/07-table

--- 01-elementary/07-table.js
+++ 01-elementary/07-table.js
@@ -1,3 +1,7 @@
+/*
+ * Print the multiplication table from 1 to 12
+ **/
+
 for (let first = 1; first <= 12; first++) {
     for (let second = 1; second <= 12; second++) {
         let product = first * second;
