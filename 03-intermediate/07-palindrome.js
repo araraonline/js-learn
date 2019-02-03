b95058ae623b7fedaceabed18a5fc74926dec63f
araraonline <andre0lra@gmail.com>
2019-02-03 11:42:10 -0200

    EXERCISES INTERMEDIATE Find palindrome (tests)

--- 03-intermediate/07-palindrome.js
+++ 03-intermediate/07-palindrome.js
@@ -40,9 +40,51 @@ function findPalindrome(string) {
 }
 
 
-console.log(findPalindrome(""));
-console.log(findPalindrome("a"));
-console.log(findPalindrome("abacate"));
-console.log(findPalindrome("abbacate"));
-console.log(findPalindrome("cateaba"));
-console.log(findPalindrome("cateabba"));
+function tests() {
+    function check(a, b) {
+        if (a !== b) {
+            throw new Error();
+        }
+    }
+
+    check(findPalindrome(""), "");
+    check(findPalindrome("a"), "a");
+    check(findPalindrome("abacate"), "aba");
+    check(findPalindrome("cateaba"), "aba");
+    check(findPalindrome("abbacate"), "abba");
+    check(findPalindrome("cateabba"), "abba");
+
+    let str = "a".repeat(99);
+    check(findPalindrome(str), str);
+    check(findPalindrome(str + "a"), str + "a");
+
+    console.log("Tests ok!");
+}
+
+function performanceTests() {
+    function check(value, message) {
+        let t1, t2;
+        t1 = Date.now();
+        findPalindrome(value);
+        t2 = Date.now();
+        console.log(`${message}: ${t2 - t1}ms`);
+    }
+
+    function randomString(size, alphabet="abcdefghijklmnopqrstuvwxyz") {
+        let string = "";
+        for (let i = 0; i < size; i++) {
+            string += alphabet[Math.floor(Math.random() * alphabet.length)];
+        }
+        return string;
+    }
+
+    console.log("Performance checks:");
+    check(randomString(1e6), "1kk-length random string (26-letter alphabet)");
+    check(randomString(1e6, "abc"), "1kk-length random string (3-letter alphabet)");
+    check(randomString(1e6, "ab"), "1kk-length random string (2-letter alphabet)");
+    check("a".repeat(1000), "1k repeated 'a'");
+    check("a".repeat(5000), "5k repeated 'a'");
+}
+
+tests();
+performanceTests();
