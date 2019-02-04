4853ebe424a17ff8cac242b86ee2bea8b076093c
araraonline <andre0lra@gmail.com>
2019-02-04 15:43:06 -0200

    EXERCISE INTERMEDIATE List interface

---
+++ 03-intermediate/08-list.js
@@ -0,0 +1,23 @@
+// I will set up an interface for a list
+
+class List {
+    constructor(memory) {
+        // construct things and set length
+    }
+
+    // modifying the structure of the array
+
+    append(value) {
+    }
+
+    pop() {
+    }
+
+    // modifying values in the array
+
+    get(index) {
+    }
+
+    set(index, value) {
+    }
+}
