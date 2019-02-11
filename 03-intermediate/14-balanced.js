9b65bb0c5d277fb1baf9e6f67f22b62d365182e9
araraonline <andre0lra@gmail.com>
2019-02-11 17:13:47 -0200

    EXERCISE INTERMEDIATE Search tree ((a, b) -- WIP)

--- 03-intermediate/14-balanced.js
+++ 03-intermediate/14-balanced.js
@@ -71,7 +71,76 @@ class LeafNode {
 }
 
 class ParentNode {
-    constructor(a, b) {
-        this.a
+    constructor(tree, parent=null) {
+        this.tree = tree;
+        this.parent = parent;
+        this.array = [];
+        this.values = [];
+    }
+
+    push(key, value) {
+        for (let i = 1; i < this.array.values; i += 2) {
+            if (key === this.array[i]) {
+                this.values[(i - 1) / 2] = value;
+                return this.tree.root;
+            } else if (key < this.array[i]) {
+                return this.array[i - 1].push(key, value);
+            }
+        }
+
+        return this.array[this.array.length - 1].push(key, value);
+    }
+
+    _overflow() {
+        if ((this.array.length - 1) / 2 + 1 > this.tree.b) {
+            let midpoint = 2 * Math.ceil((this.array.length - 3) / 4) + 1;
+            let valuesMidpoint = (midpoint - 1) / 2;
+        }
+
+        let left, middle, right;
+
+        left = new ParentNode(this.tree);
+        left.array = this.array.slice(0, midpoint);
+        left.values = this.values.slice(0, valuesMidpoint);
+
+        middle = {key: this.array[midpoint], value: this.values[valuesMidpoint]};
+
+        right = new ParentNode(this.tree);
+        right.array = this.array.slice(midpoint);
+        right.values = this.values.slice(valuesMidpoint);
+
+        if (!this.parent) {
+            this.parent = new ParentNode(this.tree);
+            this.parent.array = [left, middle.key, right];
+            this.parent.values = [middle.value];
+            return this.parent;
+        }
+
+        return this.parent._forcePush(left, middle, right);
+    }
+
+    _forcePush(left, middle, right) {
+        let {key, value} = middle;
+        for (let i = 1; i < this.array.length; i++) {
+            if (key < this.array[i]) {
+                this.array = this.array.slice(0, i + 1)
+                    .concat([left, middle.key, right])
+                    .concat(this.array.slice(i + 2));
+                this.values = this.values.slice(0, (i - 1) / 2)
+                    .concat(middle.value)
+                    .concat(this.values.slice((i - 1) / 2));
+                return this._overflow();
+            }
+        }
+
+        this.array = this.array.slice(0, -1).concat([left, middle.key, right]);
+        this.values.push(middle.value);
+        return this._overflow();
     }
 }
+
+exports.LeafNode = LeafNode;
+exports.ParentNode = ParentNode;
+exports.ABSearchTree = ABSearchTree;
+
+// TODO: set parents on _overflow's
